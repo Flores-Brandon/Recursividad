6b0e0845c7064c28614caabca99f0f11f1ba45ba
Flores-Brandon <I22050339@monclova.tecnm.mx>
2023-09-11 19:26:45 -0600

    Agregar archivos de proyecto.

---
+++ Recursividad/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5; // Cambia el número para calcular el factorial de otro valor
+            long resultado = CalcularFactorial(numero);
+            Console.WriteLine($"El factorial de {numero} es {resultado}");
+        }
+
+        static long CalcularFactorial(int n)
+        {
+            // Caso base: El factorial de 0 es 1
+            if (n == 0)
+                return 1;
+
+            // Caso recursivo: n! = n * (n-1)!
+            return n * CalcularFactorial(n - 1);
+        }
+    }
+    
+}
